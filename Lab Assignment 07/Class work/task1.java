a2f9c6e9a3bd285a121ecccb88b5c377e769c04e
Algo-Pilot <fahimasfaq01@gmail.com>
2025-08-26 23:51:27 +0600

    done

--- Lab Assignment 07/Class work/task1.java
+++ Lab Assignment 07/Class work/task1.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
-public class task01 {
-    public static void main(String [] args){
+public class task1 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("N = ");
         int num = sc.nextInt();
-        int [] arr = new int [num + 1];
-        for(int i= 0; i < num; i++){
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) {
             System.out.print("Enter a number: ");
             arr[i] = sc.nextInt();
         }
-        System.out.print("Enter another number: ");
-        arr[num] = sc.nextInt();
-        System.out.println("The elements of the array are:");
-        for(int i = 0; i < num ; i++){
-            System.out.printf("%d: %d%n",i, arr[i]);
+        System.out.println("The elements of the array are");
+        for (int i = 0; i < num; i++) {
+            System.out.printf("%d: %d%n", i, arr[i]);
         }
-        System.out.println("After resizing the array:");
-        for(int i = 0; i <= num ; i++){
-            if(i == num ){
+        System.out.println("After resizing the array: ");
+        for (int i = 0; i < num; i++) {
+            if (i == num - 1) {
                 System.out.println(arr[i]);
             }
-            else{
-                System.out.printf("%d ", arr[i]);
+            else {
+                System.out.print(arr[i] + " ");
             }
         }
-        sc.close();
     }
 }
