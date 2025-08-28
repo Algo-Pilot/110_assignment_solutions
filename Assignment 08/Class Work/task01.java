cff9a0da2091e0521d6aa3225681d306fc16241f
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-08-28 14:17:38 +0600

    Create task01.java

---
+++ Assignment 08/Class Work/task01.java
@@ -0,0 +1,13 @@
+public class task1 {
+    public static void main(String [] args){
+        evenChecker(11);
+    }
+    public static void evenChecker(int num){
+        if(num %2 ==0){
+            System.out.println("Even!!");
+        }
+        else{
+            System.out.println("Odd!!");
+        }
+    }
+}
