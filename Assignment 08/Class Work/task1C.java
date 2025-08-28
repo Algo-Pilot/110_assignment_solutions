55b73d11a14a2ebd44d83d8c8f7509281fb211d7
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-08-28 14:26:39 +0600

    Create task1C.java

---
+++ Assignment 08/Class Work/task1C.java
@@ -0,0 +1,14 @@
+public class task1C {
+    public static void main(String [] args){
+        boolean result = isPos(-5);
+        System.out.println( result );
+    }
+    public static boolean isPos(int num){
+        if(num>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
