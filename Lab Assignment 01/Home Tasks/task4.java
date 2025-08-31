22fc5081259bb07184f24acc3142b61c02d48706
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-08-31 21:49:23 +0600

    Create task4.java

---
+++ Lab Assignment 01/Home Tasks/task4.java
@@ -0,0 +1,10 @@
+public class task4 {
+    public static void main(String[] args) {
+        int a =8;
+        int b = 3;
+        int cir = 6 * a;
+        int area =(cir * b)/2;
+        System.out.println("Area: " + area);
+        System.out.println("Circumference: " + cir);
+    }
+}
