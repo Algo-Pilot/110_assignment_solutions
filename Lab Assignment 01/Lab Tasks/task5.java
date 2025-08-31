8390612fefe7e41596a113eba11cc027fc2f257d
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-08-31 19:08:52 +0600

    Create task5.java

---
+++ Lab Assignment 01/Lab Tasks/task5.java
@@ -0,0 +1,7 @@
+public class task5 {
+    public static void main(String[] args){
+        int a = 1000;
+        double result = a * 0.0254;
+        System.out.println("1000 inch is " + result + " meters.");
+    }
+}
