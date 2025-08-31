186923174d7355c1a203bb7680243cbc4ddf82ec
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-08-31 18:50:52 +0600

    Create task2_6.java

---
+++ Lab Assignment 01/Lab Tasks/task2_6.java
@@ -0,0 +1,16 @@
+public class task2_6 {
+    public static void main(String[] args){
+        String a = "5";
+        System.out.println(a);
+        String b = "6";
+        System.out.println(a+b);
+        int c = 5;
+        String d = "6";
+        System.out.println(c);
+        System.out.println(c+d);
+        String e = "5";
+        int f = 6;
+        System.out.println(e);
+        System.out.println(e+f);
+    }
+}
