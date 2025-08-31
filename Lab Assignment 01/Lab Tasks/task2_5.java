73d191ff8f3da4b682978c4eaba5998c3a604eaa
FAT CodeSmith <fahimasfaq01@gmail.com>
2025-08-31 18:44:35 +0600

    Create task2_5.java

---
+++ Lab Assignment 01/Lab Tasks/task2_5.java
@@ -0,0 +1,10 @@
+public class task2_5 {
+    public static void main(String[] args){
+        double a = 5;
+        System.out.println(a);
+        int b = 6;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(b/a);
+    }
+}
